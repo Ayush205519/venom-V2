fd818ccb56e04cc5b19a0f10569ea372547195f9
Ayush205519 <shivam23032003@gmail.com>
2024-07-29 13:35:52 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '7381988025:AAGwcJgeae3WqH2zw3BZo7k5aWFq8mY306I'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
+FORWARD_CHANNEL_ID = -1282836109
+CHANNEL_ID = -1282836109
+error_channel_id = -1282836109
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
